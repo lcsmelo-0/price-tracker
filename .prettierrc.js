49350857b3fc90208702213258c980d79d1a618a
Lucas de Melo Lima <luucasmell@gmail.com>
2023-06-16 21:47:46 -0300

    chore: Adicionar husky

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  endOfLine: 'lf',
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  jsxSingleQuote: true,
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
@@ -11,4 +11,4 @@ import { HomeComponent } from './home/home.component';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
