565feae53fe4473a64f35071de1118ff7b94ad94
Lucas de Melo Lima <luucasmell@gmail.com>
2023-06-16 22:52:52 -0300

    feat: Atualizar exibição da tabela de dados
    - Corrigir exibição dos dados da tabela para mostrar a partir do dia 0i1
    - Ajustar cálculo da variação em relação ao primeiro dia
    - Corrigir formatação da variação em relação ao primeiro dia para exibir corretamente o percentual

--- src/app/stock-details/stock-details.component.ts
+++ src/app/stock-details/stock-details.component.ts
@@ -11,7 +11,7 @@ import { MatTableDataSource } from '@angular/material/table'
 export class StockDetailsComponent implements OnInit {
   stockData: any
   symbol: string = ''
-  displayedColumns: string[] = ['date', 'open']
+  displayedColumns: string[] = ['day', 'date', 'value', 'variationD1', 'variationFirstDay']
   dataSource: MatTableDataSource<any> = new MatTableDataSource<any>([])
 
   constructor(private route: ActivatedRoute, private stockService: StockService) {}
@@ -29,21 +29,65 @@ export class StockDetailsComponent implements OnInit {
   getStockData(): void {
     this.stockService.getStockData(this.symbol).subscribe(data => {
       this.stockData = data
-      this.dataSource.data = this.stockData.chart.result[0].indicators.quote[0].open
+      const timestamps: number[] = this.stockData.chart.result[0].timestamp
+      const quotes: number[] = this.stockData.chart.result[0].indicators.quote[0].open
+      const filteredData = timestamps
+        .map((timestamp: number, index: number) => ({ timestamp, quote: quotes[index] }))
+        .filter((data: { timestamp: number; quote: number }) => data.quote !== null && data.quote !== undefined)
+        .reverse()
+        .slice(0, 30)
+
+      const formattedData = filteredData.map((data: { timestamp: number; quote: number }, index: number) => ({
+        day: index + 1,
+        date: this.formatDate(data.timestamp * 1000),
+        value: data.quote,
+        variationD1: '-',
+        variationFirstDay: '-',
+      }))
+
+      const firstDayValue = formattedData[0].value
+
+      for (let i = 1; i < formattedData.length; i++) {
+        const currentData = formattedData[i]
+        const previousData = formattedData[i - 1]
+
+        const variationD1 = ((currentData.value - previousData.value) / previousData.value) * 100
+        const variationFirstDay = ((currentData.value - firstDayValue) / firstDayValue) * 100
+
+        currentData.variationD1 = variationD1.toFixed(2) + '%'
+        currentData.variationFirstDay = variationFirstDay.toFixed(2) + '%'
+      }
+
+      this.dataSource.data = formattedData
       console.log(this.stockData)
     })
   }
 
-  calculatePriceVariation(data: any): number {
-    const firstDayOpen = data.chart.result[0].indicators.quote[0].open[0]
-    const lastDayOpen =
-      data.chart.result[0].indicators.quote[0].open[data.chart.result[0].indicators.quote[0].open.length - 1]
+  calculateVariationD1(element: any): string {
+    const currentIndex = this.dataSource.data.indexOf(element)
+    if (currentIndex === 0) {
+      return '-'
+    }
 
-    return ((lastDayOpen - firstDayOpen) / firstDayOpen) * 100
+    const previousQuote = this.dataSource.data[currentIndex - 1].quote
+    const variation = ((element.quote - previousQuote) / previousQuote) * 100
+    return `${variation.toFixed(2)}%`
+  }
+
+  calculateVariationFirstDay(element: any): string {
+    const firstDayOpen = element.chart.result[0].indicators.quote[0].open[0]
+    const currentOpen =
+      element.chart.result[0].indicators.quote[0].open[element.chart.result[0].indicators.quote[0].open.length - 1]
+    const variation = ((currentOpen - firstDayOpen) / firstDayOpen) * 100
+    return `${variation.toFixed(2)}%`
+  }
+
+  formatCurrency(value: number): string {
+    return `R$ ${value.toFixed(2)}`
   }
 
   formatDate(timestamp: number): string {
-    const date = new Date(timestamp * 1000)
+    const date = new Date(timestamp)
     return date.toLocaleDateString()
   }
 }
